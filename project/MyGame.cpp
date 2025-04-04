435696248b405945e7264bdc35d4a369cec65179
WOLNOME <143981493+WOLNOME@users.noreply.github.com>
2025-04-04 10:44:16 +0900

    余分な内容の削除

--- project/MyGame.cpp
+++ project/MyGame.cpp
@@ -16,7 +16,7 @@ void MyGame::Initialize()
 	Framework::Initialize();
 
 	//シーンマネージャーに最初のシーンをセット
-	SceneManager::GetInstance()->SetNextScene("TITLE");
+	SceneManager::GetInstance()->SetNextScene("DEVELOP");
 
 	//パーティクルエディター→PARTICLECREATOR
 }
