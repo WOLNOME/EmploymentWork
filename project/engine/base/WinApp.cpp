7b6e395158846c0eac7d807fe274e474632279bb
WOLNOME <143981493+WOLNOME@users.noreply.github.com>
2025-04-04 11:11:52 +0900

    ウィンドウ名の変更

--- project/engine/base/WinApp.cpp
+++ project/engine/base/WinApp.cpp
@@ -59,7 +59,7 @@ void WinApp::Initialize()
 	//ウィンドウの生成
 	hwnd = CreateWindow(
 		wc.lpszClassName,		//利用するクラス名
-		L"3122_ふえるぶつかり屋",  //タイトルバーの文字(何でもいい)
+		L"就職作品",				//タイトルバーの文字(何でもいい)
 		WS_OVERLAPPEDWINDOW,	//よく見るウィンドウスタイル
 		CW_USEDEFAULT,			//表示X座標(Windowsに任せる)
 		CW_USEDEFAULT,			//表示Y座標(Windowsに任せる)
