ac926908367aff1ee4ec91d30b580d59f4ab6f75
WOLNOME <k023g0109@g.neec.ac.jp>
2025-07-17 10:45:07 +0900

    スカイボックスのテクスチャを交換

--- project/application/object/environment/Skydome.cpp
+++ project/application/object/environment/Skydome.cpp
@@ -4,10 +4,11 @@
 
 void Skydome::Initialize() {
 	//変数の初期化
-	textureHandle_ = TextureManager::GetInstance()->LoadTexture("rostock_laage_airport_4k.dds");
+	textureHandle_ = TextureManager::GetInstance()->LoadTexture("skybox.dds");
 	object3d_ = std::make_unique<Object3d>();
 	object3d_->Initialize(ShapeTag{},Object3dManager::GetInstance()->GenerateName("SkyBox"),Shape::ShapeKind::kSkyBox);
-	object3d_->worldTransform.scale = { 900.0f,900.0f,900.0f };
+	object3d_->worldTransform.translate.y += 100.0f;
+	object3d_->worldTransform.scale = { 1500.0f,1500.0f,1500.0f };
 	object3d_->SetTexture(textureHandle_);
 	object3d_->SetIsLightProcess(false);
 
